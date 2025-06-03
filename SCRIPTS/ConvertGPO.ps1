97cadd30d4b2a9cbea0f70917f61d9b0c008cba8
SBBTN <bat4live@gmail.com>
2025-06-03 21:12:36 +0200

    cleaning repo

--- SCRIPTS/ConvertGPO.ps1
+++
@@ -1,3 +0,0 @@
-﻿import-module BaselineManagement
-
-ConvertFrom-GPO -Path "A:\00--G_DRIVE--\000HUMANIS_BACKUP\0_WDCE105901001\BackupGpo\201901\{6FAEF056-0025-41DD-9DB6-2244F4D7A804}" -OutputConfigurationScript
